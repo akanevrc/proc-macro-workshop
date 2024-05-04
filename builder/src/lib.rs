683aec7342889a4771b3243a364ccb2d31874845
akanevrc <akanezoranomukou@gmail.com>
2024-05-04 17:04:22 +0900

    feat: builder-07

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,14 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{
+    format_ident,
+    quote,
+};
 use syn::{
-    DeriveInput,
     parse_macro_input,
+    DeriveInput,
 };
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let fields =
@@ -26,18 +29,63 @@ pub fn derive(input: TokenStream) -> TokenStream {
                                             },
                                         },
                                     ),
-                                ) =>
-                                    match segments.first() {
-                                        Some(syn::PathSegment {
+                                ) if segments.len() == 1 =>
+                                    match &segments[0] {
+                                        syn::PathSegment {
                                             ident,
                                             ..
-                                        }) if segments.len() == 1 =>
-                                            if ident == "Option" {
-                                                (field_ident.clone(), true)
-                                            } else {
-                                                (field_ident.clone(), false)
-                                            },
-                                        _ => unimplemented!()
+                                        } => {
+                                            let is_option = ident == "Option";
+                                            let builder_ty =
+                                                if is_option {
+                                                    match &segments[0].arguments {
+                                                        syn::PathArguments::AngleBracketed(
+                                                            syn::AngleBracketedGenericArguments {
+                                                                args,
+                                                                ..
+                                                            },
+                                                        ) => {
+                                                            match args.first() {
+                                                                Some(syn::GenericArgument::Type(ty)) => ty.clone(),
+                                                                _ => unimplemented!(),
+                                                            }
+                                                        },
+                                                        _ => unimplemented!(),
+                                                    }
+                                                }
+                                                else {
+                                                    field.ty.clone()
+                                                };
+                                            let each_attr =
+                                                field.attrs.iter()
+                                                .find(|attr| attr.path().is_ident("builder"))
+                                                .and_then(|attr|
+                                                    match attr.parse_args() {
+                                                        Ok(syn::Meta::NameValue(syn::MetaNameValue {
+                                                            path: syn::Path {
+                                                                segments,
+                                                                ..
+                                                            },
+                                                            value: syn::Expr::Lit(syn::ExprLit {
+                                                                lit: syn::Lit::Str(lit_str),
+                                                                ..
+                                                            }),
+                                                            ..
+                                                        })) if segments.len() == 1 => {
+                                                            match &segments[0] {
+                                                                syn::PathSegment {
+                                                                    ident,
+                                                                    ..
+                                                                } if ident == "each" => {
+                                                                    Some(lit_str)
+                                                                },
+                                                                _ => unimplemented!(),
+                                                            }},
+                                                        _ => None,
+                                                    }
+                                                );
+                                            (field_ident.clone(), is_option, builder_ty, each_attr)
+                                        },
                                     },
                                 _ => unimplemented!(),
                             }
@@ -46,56 +94,87 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 }
             _ => unimplemented!(),
         };
-    let option_fields = fields.iter().filter(|(_, is_option)| *is_option).map(|(field, _)| field);
-    let non_option_fields = fields.iter().filter(|(_, is_option)| !*is_option).map(|(field, _)| field);
+    let command_field_names = fields.iter().map(|(ident, _, _, _)| ident);
+    let builder_field_names = command_field_names.clone();
+    let builder_field_types =
+        fields.iter()
+        .map(|(_, _, builder_ty, each_attr)|
+            match each_attr {
+                Some(_) => quote! { #builder_ty },
+                None => quote! { Option<#builder_ty> },
+            }
+        );
+    let command_field_values =
+        fields.iter()
+        .map(|(_, _, _, each_attr)|
+            match each_attr {
+                Some(_) => quote! { Vec::new() },
+                None => quote! { None },
+            }
+        );
+    let builder_method_defs =
+        fields.iter()
+        .map(|(ident, _, builder_ty, each_attr)|
+            match each_attr {
+                Some(lit) => {
+                    let method_ident = format_ident!("{}", lit.value());
+                    quote! {
+                        pub fn #method_ident(&mut self, value: String) -> &mut Self {
+                            self.#ident.push(value);
+                            self
+                        }
+                    }
+                },
+                None =>
+                    quote! {
+                        pub fn #ident(&mut self, value: #builder_ty) -> &mut Self {
+                            self.#ident = Some(value);
+                            self
+                        }
+                    },
+            }
+        );
+    let command_field_inits =
+        fields.iter()
+        .map(|(ident, is_option, _, each_attr)|
+            match (is_option, each_attr) {
+                (true, Some(_)) =>
+                    quote! {
+                        #ident: Some(self.#ident.clone()),
+                    },
+                (true, None) =>
+                    quote! {
+                        #ident: self.#ident.clone(),
+                    },
+                (false, Some(_)) =>
+                    quote! {
+                        #ident: self.#ident.clone(),
+                    },
+                (false, None) =>
+                    quote! {
+                        #ident: self.#ident.clone().ok_or(format!("{} is required", stringify!(#ident)))?,
+                    },
+            }
+        );
     quote! {
         pub struct CommandBuilder {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            #(#builder_field_names: #builder_field_types,)*
         }
 
         impl Command {
             pub fn builder() -> CommandBuilder {
                 CommandBuilder {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+                    #(#command_field_names: #command_field_values,)*
                 }
             }
         }
 
         impl CommandBuilder {
-            pub fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-
-            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-
-            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-
-            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
+            #(#builder_method_defs)*
 
             pub fn build(&self) -> Result<Command, Box<dyn Error>> {
                 Ok(Command {
-                    #(
-                        #option_fields: self.#option_fields.clone(),
-                    )*
-                    #(
-                        #non_option_fields: self.#non_option_fields.clone().ok_or(format!("{} is required", stringify!(#non_option_fields)))?,
-                    )*
+                    #(#command_field_inits)*
                 })
             }
         }
